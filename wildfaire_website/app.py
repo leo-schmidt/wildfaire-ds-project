aff5adc9e6a4f7c4c7dadcf103bf36fe844aa552
Robert Spud <robert.spud@outlook.com>
2023-06-23 15:39:43 +0100

    upgrades to show slected fires, other fires and forecast fires

--- wildfaire_website/app.py
+++ wildfaire_website/app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 import requests
-
+import time
 import folium
 import streamlit as st
 
@@ -11,17 +11,18 @@ from streamlit_folium import st_folium, folium_static
 from components.geocoding import nifc_active_fires
 from components.geocoding import geocode_address
 from components.geocoding import search_area_buffer
-from components.geocoding import nasa_fire_events
+from components.geocoding import process_wildfires
+#from components.geocoding import nasa_fire_events
 from components.geocoding import map_create
-
+from components.geocoding import pseudo_data
 
 st.set_page_config(page_title="WildfAIre version1", page_icon=":fire:", layout="wide")
 
 #load active fires from NIFC service
 active_fires = nifc_active_fires()
 
-st.header=("WildfAire - wildfire tracking application")
-st.title(":fire: WildfAire: wildfire tracking :fire:")
+st.header=("WildfAIre - wildfire tracking application")
+st.title(":fire: WildfAIre: wildfire tracking :fire:")
 
 #create data entry form in sidebar
 with st.sidebar:
@@ -53,63 +54,43 @@ if submit_button:
     if(len(geocode_result) == 1):
          st.write(geocode_result[0])
     else:
-         st.markdown('under development...creating the search area')
+
+         with st.spinner('building map results...please wait'):
+         #st.markdown('building map results...please wait')
+
          #st.write(search_area_buffer(geocode_result[0], geocode_result[1]))
-         ## create search buffer (50 km square around the address location)
-         search_poly = search_area_buffer(geocode_result[0], geocode_result[1])
-         ## call NASA event API for fire events in search area using the search square
-         st.write(search_poly['bbox'])
-         ###====== superseded ================================
-         #fire_events = nasa_fire_events(search_poly['bbox'])
-         #st.write(fire_events)
-         ###==================================================
+            ## create search buffer (50 km square around the address location)
+            search_poly = search_area_buffer(geocode_result[0], geocode_result[1], search_area)
 
-        # m = folium.Map(location=[geocode_result[0], geocode_result[1]], zoom_start=8)
-         #fire_features = pd.json_normalize(active_fires['features'])
+            ## analyse wildfire data with search area
+            selected_wildfires, other_wildfires = process_wildfires(geocode_result[0],
+                                                                    geocode_result[1],
+                                                                    active_fires,
+                                                                    search_area)
+            #selected_wildfires
+            #other_wildfires
+            if len(selected_wildfires.index) > 0:
 
-         #geo_json = folium.GeoJson(data=active_fires)
-         #geo_json.add_to(m)
-         #folium.features.GeoJson(active_fires, popup=folium.features.GeoJsonPopup(fields=['attr_IncidentShortDescription', 'poly_GISAcres'])).add_to(m)
+            ####################################################################################
+            ## send selected geopanda to wildfAIre API here
 
-         #m
+            ##pseudo model - manipulation of data
 
-         map_plot = map_create(geocode_result[0],
-                                     geocode_result[1],
-                                      search_poly['bbox'],
-                                      active_fires)
+                forecast_fires = pseudo_data(selected_wildfires)
 
+            ####################################################################################
 
-         folium_static(map_plot)
+            ## plot data and results
 
-
-
-
-
-
-#geocode_address(address_to_geocode)
-
-#1. map and geocoder
-
-    # can enter location
-    # map centred on location provided
+                map_plot = map_create(geocode_result[0],
+                                        geocode_result[1],
+                                        search_poly,
+                                        selected_wildfires,
+                                        other_wildfires,
+                                        forecast_fires)
 
 
+                folium_static(map_plot)
 
-
-#st.markdown(''':large_red_square:''')
-
-
-
-
-#2. search NASA realtime events based on location
-    #sent location to NASA API to retrieve events
-
-    #need to convert location to Km to set search area??
-
-## NASA events API for wildfires in given date range, location and categorised as open
-
-
-#3. dummy data 'forecast output'
-
-#4. Output data - distance, location, distance, travelled, icons
-            # need to embed a table wtructure here
+            else:
+                st.write('no active wildfires in search area :thumbsup:')
--- wildfaire_website/components/geocoding.py
+++ wildfaire_website/components/geocoding.py
@@ -1,16 +1,36 @@
-import requests
 import geopandas as gpd
 import pandas as pd
+import numpy as np
+
+import requests
 import shapely
 import nifc_wildfires
 import folium
 import json
+import pyproj
+
 from math import sqrt
 from shapely import wkt
-from shapely.geometry import polygon, mapping
+from shapely.ops import transform
+from shapely.geometry import Point, polygon, mapping, shape
 
+def nifc_active_fires():
+    '''
+    load data from the NIFC website using the nifc_wildfires package
+    convert raw data to geopanda data frame
+    '''
+    data = nifc_wildfires.get_active_perimeters()
+    gdf_af = gpd.GeoDataFrame.from_features(data)
+    gdf_af.set_crs('epsg:4326')
+    gdf_af['distance'] = np.nan
+
+    return gdf_af
 
 def geocode_address(input_address):
+    '''
+    Nominatim geocoder to convert the given address to lat/lon. Will return an
+    error if no result generated
+    '''
     url = "https://nominatim.openstreetmap.org"
 
     params = {
@@ -22,36 +42,123 @@ def geocode_address(input_address):
     repsonse_length = len(response)
 
     if(repsonse_length == 0):
-        return ['Could not geocode the address, please try again']
+        return [':warning: Could not geocode the address - check address and try again :warning:']
     else:
         return [response[0]['lat'], response[0]['lon']]
 
 
-
-def search_area_buffer(latitude, longitude, distance=25000/sqrt(2)):
-    #to do
+def search_area_buffer(latitude, longitude, search_area):
     '''
-    1. create a search area around house equivalent to 50 km x 50 km
+    Create a search area polygon required for selecting features for model and
+    plotting on output map.
+    reprojecting the lat/lon required to calculate search buffer in KM. May need
+    to be changed if different geographies used.
+    *2 in calculation - search area assumed to be a radius value not diameter
     '''
     gs = gpd.GeoSeries(wkt.loads(f'POINT ({longitude} {latitude})'))
-    gdf = gpd.GeoDataFrame(geometry=gs)
-    gdf.crs='EPSG:4326'
-    gdf = gdf.to_crs('EPSG:3857')
-    res = gdf.buffer(
-        distance=distance,
-        cap_style=3,
+    gdf_sa = gpd.GeoDataFrame(geometry=gs)
+    gdf_sa.crs='EPSG:4326'
+    gdf_sa = gdf_sa.to_crs('EPSG:3857')
+    res = gdf_sa.buffer(
+        distance=(search_area*2*1000)/sqrt(2)
     )
 
     search_poly = mapping(res.to_crs('EPSG:4326'))
     return search_poly
 
-'''
-calls the NASA eonet service for wildfire events.
-Good for testing but near real time and not as detailed as teh NASA FIRM data
-(see function below) which is live.
-'''
-def nasa_fire_events(search_poly):
+def process_wildfires(latitude, longitude, active_fires, search_area):
+    '''
+    perform euclidean distance analysis to select fire features for forecasting
+    '''
 
+    TransformPoint = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:32148")
+    search_centroid = TransformPoint.transform(latitude, longitude)
+    search_centroid = Point(search_centroid)
+
+    project = pyproj.Transformer.from_proj(
+        pyproj.Proj(init='epsg:4326'), # source coordinate system
+        pyproj.Proj(init='epsg:32148')) # destination coordinate system
+
+    for index, row in active_fires.iterrows():
+        #create polygon object per row to calculate distance
+        #geo: dict = {'type': row['geometry.type'], 'coordinates': row['geometry.coordinates']}
+        #polygon: Polygon = shape(geo)
+
+        polygon_feat_proj = transform(project.transform, row['geometry'])
+        #print(round(search_centroid.distance(polygon_feat_proj.boundary)/1000))
+        active_fires.at[index,'distance'] = (round(search_centroid.distance(polygon_feat_proj.boundary)/1000))
+
+    selected_wildfires = active_fires.loc[active_fires['distance'] < search_area]
+    other_wildfires = active_fires.loc[active_fires['distance'] >= search_area]
+
+    return selected_wildfires, other_wildfires
+
+
+def map_create(latitude, longitude,
+               search_poly,
+               active_fires,
+               other_wildfires,
+               forecast_fires):
+    '''
+    stitch data together for plotting on a folium map
+    map will contain:
+     1.  - search area around address
+     2.  - selected/not selected fire data
+     3.  - forecast data returned from model
+    '''
+
+    #initiate map - centred on geocode lat/lon - can we make the zoom start scaled to search area??
+    m = folium.Map(location=[latitude, longitude], zoom_start=10)
+
+    #add search area to map
+    geo_json_search = folium.GeoJson(data=search_poly, style_function=lambda x: {"fillColor": "grey", 'color': '#228B22'})
+    geo_json_search.layer_name = 'Search area'
+    m.add_child(geo_json_search)
+
+    #add selected fires to map
+    for _, r in active_fires.iterrows():
+        geo_active_fires = gpd.GeoSeries(r['geometry']).to_json()
+        geo_active_fires = folium.GeoJson(data=geo_active_fires, style_function=lambda x: {"fillColor": "red", 'color': '#FF0000'})
+        geo_active_fires.add_child(folium.Popup(r['poly_GISAcres'])) #creates popup but no content????
+        m.add_child(geo_active_fires)
+
+    #add other fires to map
+    for _, s in other_wildfires.iterrows():
+        geo_other_fires = gpd.GeoSeries(s['geometry']).to_json()
+        geo_other_fires = folium.GeoJson(data=geo_other_fires)
+        m.add_child(geo_other_fires)
+
+    #add forecast to map
+    for _, f in forecast_fires.iterrows():
+        geo_forecast_fires = gpd.GeoSeries(f['geometry']).to_json()
+        geo_forecast_fires = folium.GeoJson(data=geo_forecast_fires, style_function=lambda x: {"fillColor": "orange", 'color': '#871212'})
+
+        m.add_child(geo_forecast_fires)
+
+    #add layer control widget to map
+    folium.LayerControl().add_to(m)
+
+    #return map to app.py
+    return m
+
+
+def pseudo_data(selected_fires):
+    '''
+    manipulate polygons 'forecast'
+    '''
+    forecast_fires = selected_fires.translate(0.1,0.2)
+    forecast_fires = gpd.GeoDataFrame(forecast_fires)
+    forecast_fires.rename(columns={0: 'geometry'}, inplace=True)
+    return forecast_fires
+
+
+### DEPRECATED FUNCTION - IGNORE ###
+def nasa_fire_events(search_poly):
+    '''
+    calls the NASA eonet service for wildfire events.
+    Good for testing but only near real time and not as detailed as NASA FIRM or NIFC data
+    (see function below) which is live.
+    '''
     lon_min = search_poly[0]
     lat_min = search_poly[1]
     lon_max = search_poly[2]
@@ -72,26 +179,3 @@ def nasa_fire_events(search_poly):
         return ['No active wildfires in your search area']
     else:
         return response
-
-
-def nifc_active_fires():
-    return nifc_wildfires.get_active_perimeters()
-
-
-def map_create(latitude, longitude, search_poly, active_fires):
-
-    m = folium.Map(location=[latitude, longitude], zoom_start=8)
-    fire_features = pd.json_normalize(active_fires['features'])
-
-    geo_json = folium.GeoJson(data=active_fires)
-    geo_json.add_to(m)
-    folium.features.GeoJson(active_fires, popup=folium.features.GeoJsonPopup(fields=['attr_IncidentShortDescription', 'poly_GISAcres'])).add_to(m)
-
-    return m
-
-def nasa_firm_events(search_poly):
-    '''
-    21/06: Will work on this once technical issues at FIRM are resolved (can't genreate a map_key)
-    service desk submitted and waiting on response.
-    '''
-    1+1
