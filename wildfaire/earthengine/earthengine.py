5614ba76ef40e3a23c1a917b582d12b8a61d7930
Leonard Schmidt <leonard.sch@live.de>
2023-07-06 20:36:04 +0200

    First version of get_ee_data. Only works with dates until February 2023 lol

--- wildfaire/earthengine/earthengine.py
+++ wildfaire/earthengine/earthengine.py
@@ -1,24 +1,30 @@
 import ee
 import ee_utils
 from export_ee_data import _get_time_slices
+import numpy as np
 
-def get_ee_data(start_date,
-                geometry = ee_utils.COORDINATES['US'],
-                kernel_size = 64
+def get_ee_data(start_date = '2022-06-15',
+                geometry = ee_utils.COORDINATES['test']
     ):
 
     ### INITIALIZE GEE API ###
+
+    print('Initializing Earth Engine.')
+
     ee.Initialize()
+
+
 
     ### DEFINE CONSTANTS ###
 
     start_date = ee.Date(start_date)
 
-    sampling_scale = 1000 # meters
 
     geometry = ee.Geometry.Rectangle(geometry)
 
     ### STATIC DATA ###
+
+    print('Retrieving satellite images from EE.')
 
     # Get elevation data
     elevation = ee_utils.get_image(ee_utils.DataType.ELEVATION_SRTM)
@@ -58,29 +64,33 @@ def get_ee_data(start_date,
     image_list = [elevation, population] + time_slices[:-1]
 
     # Target (y)
-    detection = time_slices[-1]
+    detection = image_list[-1].clamp(7, 7).subtract(6).rename('detection')
 
-    #
-    arrays = ee_utils.convert_features_to_arrays(image_list, kernel_size)
+    print('Extracting arrays.')
+
+    # convert to arrays
+    arrays = ee_utils.convert_features_to_arrays(image_list, kernel_size=64)
+
+    # stack feature bands on top of detection image
     to_sample = detection.addBands(arrays)
 
-    fire_count = ee_utils.get_detection_count(
-        detection,
-        geometry=geometry,
-        sampling_scale=10 * sampling_scale,
-    )
-
-    # limit sampling to avoid EE server errors
-    sampling_limit_per_call = 1
-
-    # create samples
     samples = ee_utils.extract_samples(
-        to_sample,
-        detection_count=1,
-        geometry=geometry,
-        sampling_ratio=0,  # Only extracting examples with fire.
-        sampling_limit_per_call=sampling_limit_per_call,
-        resolution=sampling_scale,
-    )
+          to_sample,
+          detection_count=1,
+          geometry=geometry,
+          sampling_ratio=0,  # Only extracting examples with fire.
+          sampling_limit_per_call=1,
+          resolution=1000,
+      )
 
-    return samples
+    # put it in a dictionary so the different features can be retrieved by the model easily
+    # easy to see which array corresponds to which feature
+    # and to ignore features which are not needed for the model (-> feature selection)
+    feature_dict = {feature : np.array(values) for feature, values in samples.getInfo()['features'][0]['properties'].items()}
+
+    print('Complete.')
+
+    return feature_dict
+
+if __name__ == '__main__':
+    get_ee_data()
